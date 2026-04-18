7aa320a5c42e2dd0b5eb75e844941e2f55147178
Prashant Rahul <prashantrahul141@protonmail.com>
2026-04-18 22:43:36 +0530

    refactor(app): restructure applications folder
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- app/nocaprs/src/main.rs
+++ app/nocaprs/src/main.rs
@@ -1,7 +1,10 @@
+use nocaprs_platform::Platform;
+use nocaprs_platform_linux::Linux;
 use nocaprs_ui::UI;
 use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
-fn main() -> eframe::Result {
+#[tokio::main]
+async fn main() -> Result<(), eframe::Error> {
     /* setup tracing subscriber */
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let pretty_layer = fmt::layer().pretty();
@@ -10,6 +13,12 @@ fn main() -> eframe::Result {
         .with(pretty_layer)
         .init();
 
+    tokio::spawn(async {
+        let platform = Linux::default();
+        let r = platform.take_screenshot().await.unwrap();
+        dbg!(r);
+    });
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
         ..Default::default()
@@ -17,7 +26,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "nocap-rs",
         options,
-        Box::new(|cc| {
+        Box::new(|_| {
             // image support:
             // egui_extras::install_image_loaders(&cc.egui_ctx);
 
