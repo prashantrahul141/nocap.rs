f31d7b46aeb9c1ed6088bececd9a97ff77a3aa5c
Prashant Rahul <prashantrahul141@protonmail.com>
2026-04-26 16:22:25 +0530

    feat(platform): add api interface for screencapture: init and start
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- crates/platform/src/lib.rs
+++ crates/platform/src/lib.rs
@@ -1,5 +1,30 @@
 use thiserror::Error;
 
+pub trait Platform: Sized + Default {
+    fn create_screencapture_session(
+        &mut self,
+    ) -> impl std::future::Future<Output = Result<ScreenCaptureContext, ScreenCaptureError>> + Send;
+
+    fn start_screencapture(&mut self) -> impl std::future::Future<Output = Result<(), ()>> + Send;
+
+    fn take_screenshot(
+        &self,
+    ) -> impl std::future::Future<Output = Result<ScreenShot, ScreenShotError>> + Send;
+}
+
+#[derive(Debug, Error)]
+pub enum ScreenCaptureError {
+    #[error("Unknown error")]
+    Unknown,
+
+    #[cfg(target_os = "linux")]
+    #[error("DBus error")]
+    DBus(ashpd::Error),
+}
+
+#[derive(Debug)]
+pub struct ScreenCaptureContext {}
+
 #[derive(Debug, Error)]
 pub enum ScreenShotError {
     #[error("Unknown error")]
@@ -13,10 +38,4 @@ pub enum ScreenShotError {
 #[derive(Debug)]
 pub enum ScreenShot {
     Filepath(String),
-}
-
-pub trait Platform: Sized + Default {
-    fn take_screenshot(
-        &self,
-    ) -> impl std::future::Future<Output = Result<ScreenShot, ScreenShotError>> + Send;
 }
